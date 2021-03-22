88f20a12b6acc5d1e61369dc58b9b2420d8af2d6
MightyLongFall <1097089323@qq.com>
2021-03-22 23:40:59 +0800

    Update Binary Tournament Selection, mutation and crossover

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ class City:
 #   size：种群大小
 #   n：城市数
 #   m：旅行商数
+#   generation：种群的代数，即迭代次数
 #   individuals：个体集合
 #   total_d：个体总路程集合，f1(X)，X代指染色体集合
 #   balance_factor：平衡系数，f2(X)，X代指染色体集合
@@ -43,12 +44,14 @@ class City:
 #   d：每个染色体中每个旅行商的路程集合
 #   rank_list：非支配排序后的集合
 #   rank：非支配排序后每个染色体对应的等级
+#   unsQ：unshaped offspring, 即假子代，为二重列表
 class Population:
     #初始化种群
     def __init__(self, size, n, m):
         self.size = size
         self.n = n
         self.m = m
+        self.generation = 0
         self.individuals = []
         self.total_d = []
         self.balance_factor = []
@@ -58,6 +61,7 @@ class Population:
         self.sp = []
         self.rank_list = []
         self.rank = []
+        self.unsQ = []
         #产生第0代种群
         for i in range(self.size):
             chrom2 = []
@@ -103,7 +107,6 @@ class Population:
     def f2(self):
         self.balance_factor.append(max(self.d) - min(self.d))
 
-
     #快速非支配排序算子
     def fast_nondominated_sort(self):
         self.n = [0 for i in range(self.size)]     #n为支配计数(被支配)
@@ -111,7 +114,6 @@ class Population:
         self.rank_list = []     #rank_list为非支配排序后的集合
         temp = []
         self.rank = [0 for i in range(self.size)]   #rank为非支配排序后每个染色体对应的等级
-
         #计算np和sp(i,j,k,l均为计数变量)
         k = self.size-1
         for i in range(self.size-1):
@@ -126,39 +128,94 @@ class Population:
                 l += 1
             k -= 1
         #测试
-        #print(self.n)
-        #print(self.s)
+        print(self.n)
+        print(self.s)
         #sort and rank
-        times = max(self.n)
-        time = 0
-        while time < times:
-            for i in range(self.size):
+        while self.n.count(0) != 0:     #如果np的列表里依然存在0项，即还未全部变为0以下的数，就继续循环（ps：在未结束之前均存在0项，易证）
+            for i in range(self.size):  #循环搜索0项，归入temp列表
                 if self.n[i] == 0:
                     temp.append(i)
                     self.n[i] -= 1
-            if len(temp) == 0:
-                time += 1
-                continue
             self.rank_list.append(temp)
-            for chrom_dominating in temp:
+            for chrom_dominating in temp:   #将0项所支配的项np-1
                 for chrom_dominated in self.s[chrom_dominating]:
                     self.n[chrom_dominated] -= 1
             temp = []
-            time += 1
         #测试
-        #print(self.rank_list)
+        print(self.rank_list)
         for i in range(len(self.rank_list)):
             for chrom in self.rank_list[i]:
                 self.rank[chrom] = i
         #测试
-        #print(self.rank)
+        print(self.rank)
 
+    #To produce unshaped offspring:selection, crossover and mutation
+    def unshaped_evolution(self):
+        self.unsQ = []
+        while len(self.unsQ) != self.size:
+            sampleQ = random.sample(range(0, self.size), int(self.size/2))
+            srank = []
+            for sq in sampleQ:
+                srank.append(self.rank[sq])
+            srank.sort()
+            rank1 = srank[0]
+            rank2 = srank[1]
+            for sq in sampleQ:
+                i = 0
+                j = 0
+                if self.rank[sq] == rank1 and i == 0:
+                    sq1_num = sq
+                    i += 1
+                elif self.rank[sq] == rank2 and j == 0:
+                    sq2_num = sq
+                    j += 1
+                if i == 1 and j == 1:
+                    break
+            sq1 = self.individuals[sq1_num]
+            sq2 = self.individuals[sq2_num]
+            (unsq1, unsq2) = self.crossover(sq1, sq2)
+            self.unsQ.append(unsq1)
+            self.unsQ.append(unsq2)
+
+    #交叉
+    def crossover(self, chrom1, chrom2):
+        chrom1 = self.mutation(chrom1)
+        chrom2 = self.mutation(chrom2)
+        #交叉部分
+
+        return (chrom1, chrom2)
+
+    #变异
+    def mutation(self, chrom):
+        p = random.random()
+        if p > 0.5:     #对于此参数可以进行调试和优化
+            #mutate
+            p = random.random()
+            if p >= 0.5:    #前段交换
+                ex_el = random.sample(range(0, self.n-2), 2)
+                t = chrom[ex_el[0]]
+                chrom[ex_el[0]] = chrom[ex_el[1]]
+                chrom[ex_el[1]] = t
+            else:   #后端突变，需要优化，目前就直接随机数覆盖
+                mtemp = chrom[self.n-1:self.n+self.m-2]
+                position = random.randint(self.n-1, self.m + self.n - 3)
+                mp = position - (self.n - 1)
+                change = random.randint(0, self.size)
+                mtemp[mp] = change
+                while mtemp.count(change) == 2:
+                    change = random.randint(0, self.size)
+                    mtemp[mp] = change
+                #接下来是如果有反着的情况，要调换一下，否则也是无效染色体。那么这里就要考虑是否需要将后段的值优化成为前段的序号，如果要，那么前面的都需要进行调整。
+
+                chrom[position] = change
+        else:
+            return chrom
 
 if __name__ == '__main__':
     #测试代码
-    N = 16  #染色体数目
-    n = 78 #城市数目
-    m = 24  #旅行商数目
+    N = 11  #染色体数目
+    n = 16 #城市数目
+    m = 5  #旅行商数目
     city = City(n)
     for i in range(n):
         city.individuals.append([random.randint(0, 100), random.randint(0, 100)])
@@ -170,9 +227,9 @@ if __name__ == '__main__':
     #print(d)
     p = Population(N, n, m)
     print(p.individuals)
-    p.f1(city)
+    p.f1(city)      #计算两个目标函数的值
     print(p.total_d)
     print(p.balance_factor)
-    p.fast_nondominated_sort()
-
+    p.fast_nondominated_sort()  #快速非支配选择
+    #p.tournament_selection()    #二元竞赛选择
 
